60c5ee8aa6d7b0201d75809cd85cd0eb6270c118
RithvikaReddy-07 <rithvikareddy2024@gmail.com>
2025-10-10 09:00:56 +0530

    Topics Available:format method, f strings, special characters

--- day_03.py
+++ day_03.py
@@ -1,1 +1,18 @@
 #example of format method 
+name = "Rthvika"
+age = 20
+print("My name is {}, my age is {}".format(name, age))
+
+#Example of f strings
+name = "Rthvika"
+age = 20          
+print(f"My name is {name}, my age is {age}")  # it is a better alternative for the format method
+
+#printing special characters
+
+# 1)New Line
+print("Hello \n World")
+
+# 2)Tab Space
+print("Hello \t World")
+
